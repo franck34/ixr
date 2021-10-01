824df13de4fea81c7210b344f87c767f5cb2b4bc
franck34 <franck34@gmail.com>
2021-10-01 19:43:02 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 //localStorage.setItem( 'log', 'info' );
 //import 'ulog';
 
+import '../static/fonts/fontawesome-free-5.15.3-web/css/all.min.css';
+import '../static/css/main.css';
+
 import { config as worldConfig } from './config/index.js';
 import { World } from './libs/World.js';
 
